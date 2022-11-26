51095498aa87d1edb3bf6eed42012f992535f754
Nikita <sorokovsky.ni@yandex.ru>
2022-11-26 13:39:07 +0300

    Done Hometask2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Task 4
+Console.Clear() ;
+Console.Write("Введите 1-е число :");
+int a = Convert.ToInt32 (Console.ReadLine()); //ввод чисел
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32 (Console.ReadLine()); //ввод чисел
+Console.Write("Введите 3-е число: ");
+int c = Convert.ToInt32 (Console.ReadLine()); //ввод чисел
+if (a > b && a > c)
+Console.WriteLine("Nubmer Max = " + a); 
+ else if (b > c)
+Console.WriteLine("Number Max = " + b); 
+else
+Console.WriteLine("Number Max = " + c); 
+Console.ReadLine();
