1f4fa3fadb84beeb2d0f71176822d089ce588e7d
Nikita <sorokovsky.ni@yandex.ru>
2022-11-26 13:20:46 +0300

    Done Hometask1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Task 2
+Console.Clear() ;
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());// ввод чисел 
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32 (Console.ReadLine()); //ввод чисел
+if (a < b)
+ Console.Write("Yes,b = max");
+ else
+  Console.Write("NO");
+
+
